de8d41fadd177fc9ba682db601d5639648b1b7ab
maykaila <23104019@usc.edu.ph>
2026-08-19 22:53:57 +0800

    setting up

---
+++ LARGA.ManagerMobileApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LARGA.ManagerMobileApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
