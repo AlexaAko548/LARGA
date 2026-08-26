b8d9bea3ee34bdda35941875ba5ec4c88f489a60
Luigi <21800570@usc.edu.ph>
2026-08-26 20:17:43 +0800

    PreShiftChecklistPage xaml and cs

---
+++ LARGA.MobileApp/Views/PreShiftChecklistPage.xaml.cs
@@ -0,0 +1,47 @@
+using LARGA.Shared.Models;
+
+namespace LARGA.MobileApp.Views;
+
+public partial class PreShiftChecklistPage : ContentPage
+{
+    public PreShiftChecklistPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnUploadScratchesPhotoClicked(object sender, EventArgs e)
+    {
+        var photo = await MediaPicker.Default.CapturePhotoAsync();
+        if (photo != null)
+        {
+            // TODO: upload to Firebase Storage, store URL
+        }
+    }
+
+    private async void OnUploadFuelDashboardPhotoClicked(object sender, EventArgs e)
+    {
+        var photo = await MediaPicker.Default.CapturePhotoAsync();
+        if (photo != null)
+        {
+            // TODO: upload to Firebase Storage, store URL
+        }
+    }
+
+    private void OnSubmitClicked(object sender, EventArgs e)
+    {
+        var checklist = new HandoverChecklist
+        {
+            ChecklistType = ChecklistType.PreShift,
+            TireCondition = TireConditionCheckBox.IsChecked,
+            OilLevel = OilLevelCheckBox.IsChecked,
+            CoolantLevel = CoolantLevelCheckBox.IsChecked,
+            InteriorCleanliness = InteriorCleanlinessCheckBox.IsChecked,
+            ExteriorScratches = ExteriorScratchesCheckBox.IsChecked,
+            FuelVerification = FuelVerificationPicker.SelectedItem?.ToString() == "Half-tank"
+                ? FuelVerificationLevel.HalfTank
+                : FuelVerificationLevel.BelowHalfTank,
+            Timestamp = DateTime.UtcNow
+        };
+
+    }
+}
