c9e64e450e1a10b5fce6ad4e67e3519ee8763f29
Luigi <21800570@usc.edu.ph>
2026-09-01 17:58:37 +0800

    chore: Firebase initialization in MauiProgram, FirestoreService for checklist/shift/maintenance operations

--- LARGA.MobileApp/MauiProgram.cs
+++ LARGA.MobileApp/MauiProgram.cs
@@ -3,6 +3,7 @@ using LARGA.MobileApp.ViewModels;
 using LARGA.MobileApp.Views;
 using LARGA.MobileApp.Services;
 using CommunityToolkit.Maui;
+using Plugin.Firebase.Core.Platforms;
 
 namespace LARGA.MobileApp;
 
@@ -18,9 +19,17 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+			.ConfigureLifecycleEvents(events =>
+			{
+#if ANDROID
+				events.AddAndroid(android => android.OnCreate((activity, _) =>
+					CrossFirebase.Initialize(activity)));
+#endif
 			});
 
 		builder.Services.AddSingleton<IFirebaseAuthService, FirebaseAuthService>();
+		builder.Services.AddSingleton<FirestoreService>();
 		builder.Services.AddTransient<LoginViewModel>();
 		builder.Services.AddTransient<LoginPage>();
 		builder.Services.AddTransient<LandingPage>();
