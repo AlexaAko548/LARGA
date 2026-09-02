892671f5a8fc12cb09520a38af452daa49f3b832
maykaila <23104019@usc.edu.ph>
2026-09-02 16:43:00 +0800

    chore(drivermobile): handled notification for the driver side -- to be tested

--- LARGA.MobileApp/ViewModels/Driver/DriverDashboardViewModel.cs
+++ LARGA.MobileApp/ViewModels/Driver/DriverDashboardViewModel.cs
@@ -3,11 +3,26 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Maui.Controls;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.CloudMessaging;
+using Plugin.Firebase.Firestore;
+using LARGA.SharedCore.Services;
 
 namespace LARGA.MobileApp.ViewModels.Driver;
 
 public class DriverDashboardViewModel : INotifyPropertyChanged
 {
+    private readonly INotificationService _notificationService;
+
+    public DriverDashboardViewModel(INotificationService notificationService)
+    {
+        var driverId = Plugin.Firebase.Auth.CrossFirebaseAuth.Current.CurrentUser?.Uid;
+        if (!string.IsNullOrEmpty(driverId))
+        {
+            _ = notificationService.RegisterPushNotificationsAsync(driverId);
+        }
+    }
+
     private bool _isOffline = true;
     public event PropertyChangedEventHandler? PropertyChanged;
 
