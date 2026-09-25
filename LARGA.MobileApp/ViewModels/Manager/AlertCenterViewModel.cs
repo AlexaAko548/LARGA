ee6d2e946b9b23f89f3b89129cb3233ddb13283c
maykaila <23104019@usc.edu.ph>
2026-09-25 12:20:52 +0800

    removed extra {

--- LARGA.MobileApp/ViewModels/Manager/AlertCenterViewModel.cs
+++ LARGA.MobileApp/ViewModels/Manager/AlertCenterViewModel.cs
@@ -388,7 +388,7 @@ public class AlertCenterViewModel : BindableObject
         [Plugin.Firebase.Firestore.FirestoreProperty("taxiId")]
         public string? TaxiId { get; set; }
     }
-}
+
 
     private static async Task MarkAlertReadAsync(string alertId)
     {
